901c6d53586a1b1061d580ae56671e242032f118
Muhammad Usman <um337757@gmail.com>
2025-05-02 21:52:32 +0500

    reinstall recharts

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
- 
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/]
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['recharts']
+        }
+      }
+    }
+  }
 })
